dc79c99a96be6b0e04ae2d512a66399eb809abdf
pasly0920 <inwoo920621@gmail.com>
2023-03-06 15:13:57 +0900

    feat: json 요청 validate 하기 및 error handling

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import (
 	"net/http"
 )
 
+type Login struct {
+	User     string `json:"user" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
 func setupRouter() *gin.Engine {
 	router := gin.Default()
 
@@ -22,6 +27,22 @@ func setupRouter() *gin.Engine {
 			c.String(http.StatusOK, message)
 		})
 	}
+
+	// JSON 바인딩 예제 ({"user": "manu", "password": "123"})
+	router.POST("/loginJSON", func(c *gin.Context) {
+		var json Login
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if json.User != "manu" || json.Password != "123" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	})
 
 	router.POST("/post", func(c *gin.Context) {
 		id := c.Query("id")
